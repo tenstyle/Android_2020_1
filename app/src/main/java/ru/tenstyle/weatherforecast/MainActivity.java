d100add04296638c3048467e7dde2dea6fb768bd
tenstyle <tenstyle@gmail.com>
2020-05-15 10:14:45 +0300

    Готовое домашнее задание к 1 уроку. Сделал интерфейс начального экрана приложения.

--- app/src/main/java/ru/tenstyle/weatherforecast/MainActivity.java
+++ app/src/main/java/ru/tenstyle/weatherforecast/MainActivity.java
@@ -3,6 +3,21 @@ package ru.tenstyle.weatherforecast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ViewSwitcher;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +25,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+
+        String[] cities = getResources().getStringArray(R.array.cities);
+        List<String> catList = Arrays.asList(cities);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_dropdown_item_1line, catList);
+        autoCompleteTextView.setAdapter(adapter);
     }
+
 }
